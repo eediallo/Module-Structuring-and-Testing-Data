57c066f76a903108756b26e547e18f48313abe45
eediallo <elhhabdouldiallo@gmail.com>
2024-10-13 22:16:31 +0100

    updates dirPart variable

--- Sprint-1/exercises/paths.js
+++ Sprint-1/exercises/paths.js
@@ -1,3 +1,4 @@
+const { dir } = require('console');
 const exp = require('constants');
 const path = require('path')
 
@@ -18,8 +19,8 @@ const base = filePath.slice(lastSlashIndex + 1);
 console.log(`The base part of ${filePath} is ${base}`);
 
 // Create a variable to store the dir part of the filePath variable
-const dirPart = path.dirname(filePath)
+const dirPart = filePath.slice(0, lastSlashIndex)
 console.log(dirPart)
 // Create a variable to store the ext part of the variable
-const extPart = path.basename(filePath)
-console.log(extPart)
+// const extPart = path.basename(filePath)
+// console.log(extPart)
