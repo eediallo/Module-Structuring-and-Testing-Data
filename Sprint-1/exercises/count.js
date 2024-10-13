6ad26ff1cd01def83a31f4bc4176637627dfcfe3
eediallo <elhhabdouldiallo@gmail.com>
2024-10-13 21:26:35 +0100

    describe what line three is doing

--- Sprint-1/exercises/count.js
+++ Sprint-1/exercises/count.js
@@ -4,3 +4,7 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+// Line 3 is an assignment statement, it is updating the value of the count variable by adding 1 to its current value
+// Can also say that line 3 is incrementing the count variable by 1
+// Or it is a reassignment statement, updating the value of the count variable
+// it can also be written as count += 1; or count++;
