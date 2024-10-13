58b728b4841c46567562d784d8974c1326715dfb
eediallo <elhhabdouldiallo@gmail.com>
2024-10-13 21:33:15 +0100

    creates roundedNum variable

--- Sprint-1/exercises/decimal.js
+++ Sprint-1/exercises/decimal.js
@@ -9,5 +9,6 @@ console.log("whole number part", wholeNumberPart);
 const decimalPart = num - wholeNumberPart;
 console.log("decimal part = ",decimalPart);
 // Create a variable called roundedNum and assign to it an expression that evaluates to 57 ( num rounded to the nearest whole number )
-
+const roundedNum = Math.ceil(num);
+console.log("rounded number = ",roundedNum);
 // Log your variables to the console to check your answers
