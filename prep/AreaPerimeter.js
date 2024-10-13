120b7ec00db5814320b95bc6a5a398790157e350
eediallo <elhhabdouldiallo@gmail.com>
2024-10-13 21:11:26 +0100

    calculates area of and perimeter of a rectangle

---
+++ prep/AreaPerimeter.js
@@ -0,0 +1,12 @@
+const height = 10;
+const width = 30;
+
+
+const area = height * width;
+
+const perimeter = 2 * (height + width);
+
+console.log("are = ",area); 
+console.log("perimeter = ", perimeter);
+
+
