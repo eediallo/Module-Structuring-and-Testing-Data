c7a7ad9ff92536c078e26b9ee715f97338298eb2
eediallo <elhhabdouldiallo@gmail.com>
2024-10-15 06:49:09 +0100

    changes 'const' to 'let' .

--- Sprint-1/errors/1.js
+++ Sprint-1/errors/1.js
@@ -1,4 +1,7 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
+//const age = 33; ==> We cannot reassign a constant value instead we use the "let" keyword or "var"(not recommended)
+const age = 33
 age = age + 1;
+
+//console.log(age)
