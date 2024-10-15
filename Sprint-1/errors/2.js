3bedbb68a6cb84f222f544815e6380707826e19b
eediallo <elhhabdouldiallo@gmail.com>
2024-10-15 06:54:56 +0100

    moves cityOfBirth variable up

--- Sprint-1/errors/2.js
+++ Sprint-1/errors/2.js
@@ -1,5 +1,9 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
+// The program executes from top down, so cityOfBirth needs to come first, otherwise we won't have access to it
+// console.log(`I was born in ${cityOfBirth}`);
+// const cityOfBirth = "Bolton";
+
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
