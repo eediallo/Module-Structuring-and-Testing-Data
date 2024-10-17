d920373fa814fbf693b0bcc1720744689b4b60c2
eediallo <elhhabdouldiallo@gmail.com>
2024-10-17 18:09:27 +0100

    debug -> replaces console.log with the 'return' keyword

--- Sprint-2/debug/0.js
+++ Sprint-2/debug/0.js
@@ -1,7 +1,8 @@
 // Predict and explain first...
 
 function multiply(a, b) {
-  console.log(a * b);
+ // console.log(a * b);// console.log() does not have a return value.
+ return (a * b)
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
