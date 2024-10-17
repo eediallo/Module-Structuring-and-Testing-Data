f2ed6883eec7529d1cc5a59cc2589ffd0568abe4
eediallo <elhhabdouldiallo@gmail.com>
2024-10-17 18:31:14 +0100

    errors --> set default 0.5 as default value of decimalNumber arg and call right function

--- Sprint-2/errors/1.js
+++ Sprint-2/errors/1.js
@@ -3,11 +3,13 @@
 // Why will an error occur when this program runs?
 // Try playing computer with the example to work out what is going on
 
-function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
+function convertToPercentage(decimalNumber = 0.5) {
+ // const decimalNumber = 0.5  ==> declared already
   const percentage = `${decimalNumber * 100}%`;
-
   return percentage;
 }
 
-console.log(decimalNumber);
+// wrong function call
+//console.log(decimalNumber);
+
+console.log(convertToPercentage())
