3ca213716a8a082f0bfbd38ae794d014e6319ac5
eediallo <elhhabdouldiallo@gmail.com>
2024-10-17 17:35:19 +0100

    adds param time to function

--- Sprint-2/prep/assertFormatDate.js
+++ Sprint-2/prep/assertFormatDate.js
@@ -1,11 +1,14 @@
-function formatAs12HourClock(){} // does not return any value
-console.assert(formatAs12HourClock("08:00") === "08:00 am")
+// function formatAs12HourClock(){} // does not return any value
+// console.assert(formatAs12HourClock("08:00") === "08:00 am")
 
 
-console.assert(formatAs12HourClock("08:00") === "08:00 am",
-    `Current output: ${formatAs12HourClock("08:00")}, target output: 08:00 am`
-)
+// console.assert(formatAs12HourClock("08:00") === "08:00 am",
+//     `Current output: ${formatAs12HourClock("08:00")}, target output: 08:00 am`
+// )
 
+function formatAs12HourClock(time){
+    return `${time} am`;
+}
 
 const currentOutput = formatAs12HourClock("08:00")
 const targetOutput = "08:00 am"
