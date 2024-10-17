f68eb03caceddd49653a658311fbbaea21af0613
eediallo <elhhabdouldiallo@gmail.com>
2024-10-17 09:59:02 +0100

    adds additional argument to console assert

--- Sprint-2/prep/assertFormatDate.js
+++ Sprint-2/prep/assertFormatDate.js
@@ -1,2 +1,7 @@
 function formatAs12HourClock(){} // does not return any value
 console.assert(formatAs12HourClock("08:00") === "08:00 am")
+
+
+console.assert(formatAs12HourClock("08:00") === "08:00 am",
+    `Current output: ${formatAs12HourClock("08:00")}, target output: 08:00`
+)
