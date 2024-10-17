cc44c64aa53853159d082958857f04733972c90a
eediallo <elhhabdouldiallo@gmail.com>
2024-10-17 20:56:39 +0100

    interpret  -->  answers questions of time-format

--- Sprint-2/interpret/time-format.js
+++ Sprint-2/interpret/time-format.js
@@ -2,6 +2,8 @@ function pad(num) {
   return num.toString().padStart(2, "0");
 }
 
+console.log(pad(5))
+
 function formatTimeDisplay(seconds) {
   const remainingSeconds = seconds % 60;
   const totalMinutes = (seconds - remainingSeconds) / 60;
@@ -13,19 +15,27 @@ function formatTimeDisplay(seconds) {
   )}`;
 }
 
+console.log(formatTimeDisplay(61))
+
 // You will need to play computer with this example - use the Python Visualiser https://pythontutor.com/visualize.html#mode=edit
 // to help you answer these questions
 
 // Questions
 
 // a) When formatTimeDisplay is called how many times will pad be called?
+// 3 times
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
+// it is 0 because the is no remaining hours
 
 // c) What is the return value of pad is called for the first time?
+ // it is '00'
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
+//it is 1 because the remaining value of seconds is 1 and the function pad takes it as an argument
+
 
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
+// it is '01' because the padding length is 2. Num is converted into a string first before the padding is applied
