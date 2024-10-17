daecc3787fe82381a5566c66b324e23dfb52d290
eediallo <elhhabdouldiallo@gmail.com>
2024-10-17 18:25:42 +0100

    errors --> removes double declaration of 'str' string

--- Sprint-2/errors/0.js
+++ Sprint-2/errors/0.js
@@ -1,9 +1,12 @@
 // Predict and explain first...
+// str is declared twice. 
+
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
+  return `${str[0].toUpperCase()}${str.slice(1)}`;
 }
+
+console.log(capitalise("abdoul"))
