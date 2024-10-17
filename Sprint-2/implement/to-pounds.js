b52e1dbb373f9b7ec0809c95e7847ace254ce30e
eediallo <elhhabdouldiallo@gmail.com>
2024-10-17 20:09:15 +0100

    implement -->  implements toPounds function

--- Sprint-2/implement/to-pounds.js
+++ Sprint-2/implement/to-pounds.js
@@ -4,3 +4,37 @@
 // You will need to declare a function called toPounds with an appropriately named parameter.
 
 // You should call this function a number of times to check it works for different inputs
+
+//assertArg check the validity of the data type pass as an argument
+function assertArg(arg) {
+    if (typeof arg !== "string") {
+        throw new Error(`"${arg}" must be a string`);
+    }
+}
+
+const toPounds = function (moneyInPence) {
+  //check validity of moneyInPence
+  assertArg(moneyInPence);
+
+  
+  const penceStringWithoutTrailingP = moneyInPence.substring(
+    0,
+    moneyInPence.length - 1
+  );
+
+  const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+  const pounds = paddedPenceNumberString.substring(
+    0,
+    paddedPenceNumberString.length - 2
+  );
+
+  const pence = paddedPenceNumberString
+    .substring(paddedPenceNumberString.length - 2)
+    .padEnd(2, "0");
+
+  return `£${pounds}.${pence}`;
+};
+
+// console.log(toPounds("399p"));
+// console.log(toPounds("1240999"));
+// console.log(toPounds("1_500_987"));
