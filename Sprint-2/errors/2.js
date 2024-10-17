b8ad5b117a89efba2893e0144a5e6443f8323d2f
eediallo <elhhabdouldiallo@gmail.com>
2024-10-17 18:34:56 +0100

    errors --> passes num as arg and removes 3

--- Sprint-2/errors/2.js
+++ Sprint-2/errors/2.js
@@ -1,10 +1,16 @@
 
-// Predict and explain first...
+// Predict and explain first...#
+// 3 is not associated with a variable
+// num is not declared
 
 // this function should square any number but instead we're going to get an error
 
-function square(3) {
+function square(num) {
     return num * num;
 }
 
 
+
+// console.log(square(3))
+// console.log(square(9))
+// console.log(square(10))
