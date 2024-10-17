07853c2a317ecf80534d2d4be4a723a4fd6337bf
eediallo <elhhabdouldiallo@gmail.com>
2024-10-17 17:41:27 +0100

    adds two other failed assertions

--- Sprint-2/prep/assertFormatDate.js
+++ Sprint-2/prep/assertFormatDate.js
@@ -10,9 +10,29 @@ function formatAs12HourClock(time){
     return `${time} am`;
 }
 
+
+
+// first assertion
 const currentOutput = formatAs12HourClock("08:00")
 const targetOutput = "08:00 am"
 
 console.assert(currentOutput === targetOutput, 
     `current output: ${currentOutput}, target output: ${targetOutput}`
+)
+
+// second assertion
+const currentOutput2 = formatAs12HourClock("23:00")
+const targetOutput2 = "11:00 pm"
+
+
+console.assert(currentOutput2 === targetOutput2, 
+    `current output: ${currentOutput2}, target output: ${targetOutput2}`
+)
+
+// third assertion
+const currentOutput3 = formatAs12HourClock("14:00")
+const targetOutput3 = "2:00 pm"
+
+console.assert(currentOutput3 === targetOutput3, 
+    `current output: ${currentOutput3}, target output: ${targetOutput3}`
 )
