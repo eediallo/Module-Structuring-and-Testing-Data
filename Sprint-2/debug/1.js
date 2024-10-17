6fa86f5919eb33f071c22bb9d87c6af43158b92f
eediallo <elhhabdouldiallo@gmail.com>
2024-10-17 18:12:53 +0100

    debug -> returns a + b instead of return; which terminates the function earlier

--- Sprint-2/debug/1.js
+++ Sprint-2/debug/1.js
@@ -1,8 +1,8 @@
 // Predict and explain first...
+// the return keyword terminates the function earlier. The a+b calculation will not be reached
 
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
