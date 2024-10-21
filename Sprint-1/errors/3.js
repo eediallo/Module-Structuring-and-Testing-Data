78e2c59c738afeed7d2b6b68b2b9b1d3112e0759
eediallo <elhhabdouldiallo@gmail.com>
2024-10-22 00:43:43 +0100

    changes manual conversion of cardNumber variable

--- Sprint-1/errors/3.js
+++ Sprint-1/errors/3.js
@@ -12,6 +12,6 @@
 // One way to solve this is:
 // 1. Convert carNumber into a string
 // 2. subtracts 4 to the length of cardNumber to be able to extract the last 4 elements since we not dealing with and array
-const cardNumber = "4533787178994213";
-const last4Digits = +cardNumber.slice(cardNumber.length- 4);
-//console.log(last4Digits)
+const cardNumber = 4533787178994213;
+const last4Digits = +cardNumber.toString().slice(- 4);
+// console.log(last4Digits)
