519aa9afb331afc45e6822ef176214f580beab7e
eediallo <elhhabdouldiallo@gmail.com>
2024-10-21 17:49:31 +0100

    removes numeric values at the beginning of variables names

--- Sprint-1/errors/4.js
+++ Sprint-1/errors/4.js
@@ -1,2 +1,14 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+// const 12HourClockTime = "20:53";
+// const 24hourClockTime = "08:53";
+
+// we cannot start a variable name with a numeric value
+// const 12HourClockTime = "20:53";
+// const 24hourClockTime = "08:53";
+// instead we can do:
+
+const twelveHourClockTime = "20:53";
+const twentyFourHourClockTime = "08:53";
+
+// or 
+const _12HourClockTime = "20:53";
+const _24hourClockTime = "08:53";
